995226bc61a9e227613500314d08d291728ed759
metajake <jaynoco@gmail.com>
2019-02-23 13:19:47 -0600

    Added Channels (app).

--- algfapp/urls.py
+++ algfapp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('productionschedule.urls')),
-    path('', include('pandaspreadsheet.urls'))
+    path('', include('pandaspreadsheet.urls')),
+    url(r'^channel/', include('channel.urls')),
 ]
