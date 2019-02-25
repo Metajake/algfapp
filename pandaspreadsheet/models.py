8c3114d695eebf729ce46b6cfb5fd23eedd12238
metajake <jaynoco@gmail.com>
2019-02-25 09:24:21 -0600

    Begin Product model.

--- pandaspreadsheet/models.py
+++ pandaspreadsheet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    item_number = models.TextField()
+    customer = models.TextField(blank=True)
+    production_date = models.DateField(blank=True)
+    kettle_number = models.TextField(choices=(
+        ('K1','Kettle 1'),
+        ('K2','Kettle 2'),
+        ('K3','Kettle 3'),
+    ), blank=True)
