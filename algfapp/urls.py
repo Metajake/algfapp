e13dfce5856b0d96554b8ec726c436378c220615
metajake <jaynoco@gmail.com>
2019-05-01 09:48:10 -0500

    Updated Nav Admin Link relativity.

--- algfapp/urls.py
+++ algfapp/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('productionschedule.urls')),
+    path('prod/', include('productionschedule.urls')),
     path('', include('pandaspreadsheet.urls')),
     url(r'^channel/', include('channel.urls')),
 ]
