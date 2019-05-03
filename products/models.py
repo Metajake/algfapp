8365b41ace868958e3826eb6cc74454c8d38562e
metajake <jaynoco@gmail.com>
2019-05-03 11:06:13 -0500

    Added Products App, Updated Production Form.xls

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    item_number = models.CharField(max_length=10)
+    product_name = models.CharField(max_length = 100)
+    gluten_free = models.BooleanField(default = False)
+    usda_product = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.item_number + ' ' + self.product_name
