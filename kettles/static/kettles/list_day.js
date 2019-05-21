59a8c42882e5fd77af5ba99b5ad41d57c4ea8912
metajake <jaynoco@gmail.com>
2019-05-21 12:00:19 -0500

    Kettles List increase font size of First Incomplete Product to make.

--- kettles/static/kettles/list_day.js
+++ kettles/static/kettles/list_day.js
@@ -6,6 +6,23 @@ chatSocket.onopen = function(event) {
   console.log("WebSocket is open now.");
 };
 
+function findFirstProductToMake(){
+  $('.product-list').each(function(index, item){
+    var firstProduct = $(item).find('.product-item.is-complete + .product-item:not(".is-complete")');
+    console.log(firstProduct.length)
+    if(firstProduct.length){
+      firstProduct.each(function(index2, item2){
+        $(item2).find('p.product-schedule-number').css('font-size', '5vh')
+        return false;
+      })
+    }else(
+      $(item).find('.product-item:not(".is-complete"):first-child').find('p.product-schedule-number').css('font-size', '5vh')
+    );
+  })
+}
+
+findFirstProductToMake();
+
 chatSocket.onmessage = function(e) {
     console.log("GOT MESSAqge");
     var data = JSON.parse(e.data);
@@ -18,6 +35,7 @@ chatSocket.onmessage = function(e) {
       success: function(data){
         $('#production-list').html('');
         $('#production-list').html(data);
+        findFirstProductToMake();
       },
     })
 };
