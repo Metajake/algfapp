3421b1ba99775df26f13e4c2e041e7f44ef19942
metajake <jaynoco@gmail.com>
2019-05-25 11:38:10 -0500

    Added Date to Kettles Production Day List. Production Day admin shows Products per day.

--- kettles/admin.py
+++ kettles/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import ProductionDay, Kettle, Product
 
-admin.site.register(ProductionDay)
+class ProductInline(admin.TabularInline):
+    model = Product
+
+class ProductionDayAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+
+admin.site.register(ProductionDay, ProductionDayAdmin)
 admin.site.register(Kettle)
 admin.site.register(Product)
