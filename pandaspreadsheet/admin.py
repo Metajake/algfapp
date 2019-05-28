11894eff49dd320d76d7dca57218aba89e958885
metajake <jaynoco@gmail.com>
2019-05-28 12:09:47 -0500

    Kettle List Day added Gluten Free color code.

--- pandaspreadsheet/admin.py
+++ pandaspreadsheet/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from .models import Product, Kettle, CalendarDay
 
-admin.site.register(Product)
+# admin.site.register(Product)
 # admin.site.register(Kettle)
-admin.site.register(CalendarDay)
+# admin.site.register(CalendarDay)
--- productionschedule/admin.py
+++ productionschedule/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import Product, CalendarDay
 
-admin.site.register(Product)
-admin.site.register(CalendarDay)
+# admin.site.register(Product)
+# admin.site.register(CalendarDay)
