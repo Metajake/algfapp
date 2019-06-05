dc370346d06dfbad6711cc7fbd373326856918ac
metajake <jaynoco@gmail.com>
2019-06-05 13:28:54 -0500

    Products Product model item number unique.

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    item_number = models.CharField(max_length=10)
+    item_number = models.CharField(max_length=10, unique=True)
     product_name = models.CharField(max_length = 100)
     gluten_free = models.BooleanField(default = False)
     usda_product = models.BooleanField(default = False)
