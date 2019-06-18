74e00cd0e0f05bb536e957744daa2e7d29f4c0b5
metajake <jaynoco@gmail.com>
2019-06-18 15:39:48 -0500

    Refactor Kettle Static file structure.

--- kettles/static/kettles/js/list_day_websocket.js
+++ kettles/static/kettles/js/list_day_websocket.js
@@ -32,30 +32,3 @@ function startWebsocket(websocketServerLocation){
 }
 
 startWebsocket('ws://' + window.location.host + '/ws/kettles/')
-
-//Toggle Display from Production Week to Production Day
-function showProductionDay(duration){
-  setTimeout(function(){
-    $('#slide-week').fadeToggle(1000, function(){
-      $('#slide-day').fadeToggle(1000)
-    })
-    showProductionWeek(duration)
-  }, duration*.15)//5 minutes*.15=45 seconds
-}
-//Toggle Display from Production Day to Production Week
-function showProductionWeek(duration){
-  setTimeout(function(){
-    $('#slide-day').fadeToggle(1000, function(){
-      $('#slide-week').fadeToggle(1000)
-    })
-    showProductionDay(duration)
-  }, duration)
-}
-
-if(window.location.hostname !== "10.0.1.164"){
-  // $('#slide-day').css('display', 'none')
-  // $('#slide-week').css('display', 'block')
-  showProductionWeek(10000)
-}else{
-  showProductionWeek(300000) //five minutes
-}
